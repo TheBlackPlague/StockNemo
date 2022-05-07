4dd5a4e61465561b3ae2054ae2772c3270c6d77e
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-05-07 12:49:01 +0500

    Implement BlackMagicBitBoard to generate moves fast for sliding pieces.

--- Backend/Move/BitLegalMoveSet.cs
+++ Backend/Move/BitLegalMoveSet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Backend.Board;
 
 namespace Backend.Move
@@ -6,95 +7,7 @@ namespace Backend.Move
 
     public class BitLegalMoveSet
     {
-
-        private const int ROOK_BITS = 12;
-        private const int BISHOP_BITS = 9;
-
-        private static readonly (BitBoard, int)[,] RookMagicData = {
-            {
-                (0x80280013FF84FFFF, 10890), (0x5FFBFEFDFEF67FFF, 50579), (0xFFEFFAFFEFFDFFFF, 62020),
-                (0x003000900300008A, 67322), (0x0050028010500023, 80251), (0x0020012120A00020, 58503),
-                (0x0030006000C00030, 51175), (0x0058005806B00002, 83130)
-            },
-            {
-                (0x7FBFF7FBFBEAFFFC, 50430), (0x0000140081050002, 21613), (0x0000180043800048, 72625), 
-                (0x7FFFE800021FFFB8, 80755), (0xFFFFCFFE7FCFFFAF, 69753), (0x00001800C0180060, 26973), 
-                (0x4F8018005FD00018, 84972), (0x0000180030620018, 31958)
-            },
-            {
-                (0x00300018010C0003, 69272), (0x0003000C0085FFFF, 48372), (0xFFFDFFF7FBFEFFF7, 65477), 
-                (0x7FC1FFDFFC001FFF, 43972), (0xFFFEFFDFFDFFDFFF, 57154), (0x7C108007BEFFF81F, 53521), 
-                (0x20408007BFE00810, 30534), (0x0400800558604100, 16548)
-            },
-            {
-                (0x0040200010080008, 46407), (0x0010020008040004, 11841), (0xFFFDFEFFF7FBFFF7, 21112),
-                (0xFEBF7DFFF8FEFFF9, 44214), (0xC00000FFE001FFE0, 57925), (0x4AF01F00078007C3, 29574),
-                (0xBFFBFAFFFB683F7F, 17309), (0x0807F67FFA102040, 40143)
-            },
-            {
-                (0x200008E800300030, 64659), (0x0000008780180018, 70469), (0x0000010300180018, 62917), 
-                (0x4000008180180018, 60997), (0x008080310005FFFA, 18554), (0x4000188100060006, 14385), 
-                (0xFFFFFF7FFFBFBFFF,     0), (0x0000802000200040, 38091)
-            },
-            {
-                (0x20000202EC002800, 25122), (0xFFFFF9FF7CFFF3FF, 60083), (0x000000404B801800, 72209), 
-                (0x2000002FE03FD000, 67875), (0xFFFFFF6FFE7FCFFD, 56290), (0xBFF7EFFFBFC00FFF, 43807), 
-                (0x000000100800A804, 73365), (0x6054000A58005805, 76398)
-            },
-            {
-                (0x0829000101150028, 20024), (0x00000085008A0014,  9513), (0x8000002B00408028, 24324),
-                (0x4000002040790028, 22996), (0x7800002010288028, 23213), (0x0000001800E08018, 56002),
-                (0xA3A80003F3A40048, 22809), (0x2003D80000500028, 44545)
-            },
-            {
-                (0xFFFFF37EEFEFDFBE, 36072), (0x40000280090013C1,  4750), (0xBF7FFEFFBFFAF71F,  6014), 
-                (0xFFFDFFFF777B7D6E, 36054), (0x48300007E8080C02, 78538), (0xAFE0000FFF780402, 28745), 
-                (0xEE73FFFBFFBB77FE,  8555), (0x0002000308482882,  1009)
-            }
-        };
-        private static readonly (BitBoard, int)[,] BishopMagicData = {
-            {
-                (0xA7020080601803D8, 60984), (0x13802040400801F1, 66046), (0x0A0080181001F60C, 32910),
-                (0x1840802004238008, 16369), (0xC03FE00100000000, 42115), (0x24C00BFFFF400000, 835),
-                (0x0808101F40007F04, 18910), (0x100808201EC00080, 25911)
-            },
-
-            {
-                (0xFFA2FEFFBFEFB7FF, 63301), (0x083E3EE040080801, 16063), (0xC0800080181001F8, 17481),
-                (0x0440007FE0031000, 59361), (0x2010007FFC000000, 18735), (0x1079FFE000FF8000, 61249),
-                (0x3C0708101F400080, 68938), (0x080614080FA00040, 61791)
-            },
-            {
-                (0x7FFE7FFF817FCFF9, 21893), (0x7FFEBFFFA01027FD, 62068), (0x53018080C00F4001, 19829),
-                (0x407E0001000FFB8A, 26091), (0x201FE000FFF80010, 15815), (0xFFDFEFFFDE39FFEF, 16419),
-                (0xCC8808000FBF8002, 59777), (0x7FF7FBFFF8203FFF, 16288)
-            },
-            {
-                (0x8800013E8300C030, 33235), (0x0420009701806018, 15459), (0x7FFEFF7F7F01F7FD, 15863),
-                (0x8700303010C0C006, 75555), (0xC800181810606000, 79445), (0x20002038001C8010, 15917),
-                (0x087FF038000FC001, 8512), (0x00080C0C00083007, 73069)
-            },
-            {
-                (0x00000080FC82C040, 16078), (0x000000407E416020, 19168), (0x00600203F8008020, 11056),
-                (0xD003FEFE04404080, 62544), (0xA00020C018003088, 80477), (0x7FBFFE700BFFE800, 75049),
-                (0x107FF00FE4000F90, 32947), (0x7F8FFFCFF1D007F8, 59172)
-            },
-            {
-                (0x0000004100F88080, 55845), (0x00000020807C4040, 61806), (0x00000041018700C0, 73601),
-                (0x0010000080FC4080, 15546), (0x1000003C80180030, 45243), (0xC10000DF80280050, 20333),
-                (0xFFFFFFBFEFF80FDC, 33402), (0x000000101003F812, 25917)
-            },
-            {
-                (0x0800001F40808200, 32875), (0x084000101F3FD208, 4639), (0x080000000F808081, 17077),
-                (0x0004000008003F80, 62324), (0x08000001001FE040, 18159), (0x72DD000040900A00, 61436),
-                (0xFFFFFEFFBFEFF81D, 57073), (0xCD8000200FEBF209, 61025)
-            },
-            {
-                (0x100000101EC10082, 81259), (0x7FBAFFFFEFE0C02F, 64083), (0x7F83FFFFFFF07F7F, 56114),
-                (0xFFF1FFFFFFF7FFC1, 57058), (0x0878040000FFE01F, 58912), (0x945E388000801012, 22194),
-                (0x0840800080200FDA, 70880), (0x100000C05F582008, 11140)
-            }
-        };
+        
         private static readonly BitBoard[,] WhitePawnAttacks = {
             { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 },
             { 0x20000, 0x50000, 0xa0000, 0x140000, 0x280000, 0x500000, 0xa00000, 0x400000 },
@@ -167,10 +80,7 @@ namespace Backend.Move
                 0x0044280000000000, 0x0088500000000000, 0x0010A00000000000, 0x0020400000000000
             }
         };
-        private static readonly (BitBoard, BitBoard, int)[,] RookMagic =
-            new (BitBoard, BitBoard, int)[BitDataBoard.UBOUND, BitDataBoard.UBOUND];
-        private static readonly (BitBoard, BitBoard, int)[,] BishopMagic =
-            new (BitBoard, BitBoard, int)[BitDataBoard.UBOUND, BitDataBoard.UBOUND];
+        
         private static readonly BitBoard[] SlidingMoves = new BitBoard[87988];
         
         private readonly BitDataBoard Board;
@@ -180,78 +90,49 @@ namespace Backend.Move
         
         public int Count => Moves.Count;
         private BitBoard Moves = BitBoard.Default;
-        
-        private static BitBoard GetRookBlockers(int v, int h)
+
+        private static void GenerateSlidingMoves(Piece piece)
         {
-            BitBoard hMoves = BitBoard.Hs[h] & ~(BitBoard.Vs[0] | BitBoard.Vs[7]);
-            BitBoard vMoves = BitBoard.Vs[v] & ~(BitBoard.Hs[0] | BitBoard.Hs[7]);
-
-            return (hMoves | vMoves) & ~(BitBoard)(h, v);
-        }
-        private static BitBoard GetBishopBlockers(int v, int h)
-        {
-            BitBoard rays = BitBoard.Default;
-            int i = 0;
-            while (i < 64) {
-                int hI = i % 8;
-                int vI = i / 8;
-
-                int hD = Math.Abs(h - hI);
-                int vD = Math.Abs(v - vI);
-
-                if (hD == vD && vD != 0) rays |= 1UL << i;
-
-                i++;
-            }
-
-            return rays & ~BitBoard.Edged;
-        }
-
-        private static void GenerateRookMasks()
-        {
+            ((BitBoard, BitBoard, int)[,], int) args = piece switch
+            {
+                Piece.Rook => (MagicBitBoard.RookMagic, MagicBitBoard.ROOK),
+                Piece.Bishop => (MagicBitBoard.BishopMagic, MagicBitBoard.BISHOP),
+                Piece.Pawn or Piece.Knight or Piece.Queen or Piece.King or Piece.Empty or _ => 
+                    throw new InvalidDataException("No magic table found.")
+            };
+            (int, int)[] deltas = piece switch
+            {
+                Piece.Rook => new[]
+                {
+                    (1, 0),
+                    (0, -1),
+                    (-1, 0),
+                    (0, 1)
+                },
+                Piece.Bishop => new[]
+                {
+                    (1, 1),
+                    (1, -1),
+                    (-1, -1),
+                    (-1, 1)
+                },
+                Piece.Pawn or Piece.Knight or Piece.Queen or Piece.King or Piece.Empty or _ =>
+                    throw new InvalidDataException("No magic table found.")
+            };
+            
             for (int h = 0; h < BitDataBoard.UBOUND; h++)
             for (int v = 0; v < BitDataBoard.UBOUND; v++) {
-                (BitBoard magic, int offset) = RookMagicData[v, h];
-                RookMagic[v, h] = (magic, ~GetRookBlockers(v, h), offset);
-                Console.WriteLine("Generated rook magic: " + (h, v));
-            }
-        }
-        
-        private static void GenerateBishopMasks()
-        {
-            for (int h = 0; h < BitDataBoard.UBOUND; h++)
-            for (int v = 0; v < BitDataBoard.UBOUND; v++) {
-                (BitBoard magic, int offset) = BishopMagicData[v, h];
-                BishopMagic[v, h] = (magic, ~GetBishopBlockers(v, h), offset);
-                Console.WriteLine("Generated bishop magic: " + (h, v));
-            }
-        }
-
-        private static int GetMagicIndex(
-            (BitBoard, BitBoard, int)[,] magics, int indexBits, BitBoard blockers, int h, int v
-        )
-        {
-            (BitBoard magic, BitBoard mask, int offset) = magics[v, h];
-            BitBoard relevantBlockers = blockers | mask;
-            BitBoard hash = relevantBlockers * magic;
-            return offset + (int)(ulong)(hash >> (64 - indexBits));
-        }
-
-        private static void GenerateSlidingMoves(
-            BitBoard[] moveTable, (BitBoard, BitBoard, int)[,] magics, int indexBits, (int, int)[] deltas
-        )
-        {
-            for (int h = 0; h < BitDataBoard.UBOUND; h++)
-            for (int v = 0; v < BitDataBoard.UBOUND; v++) {
-                (_, BitBoard mask, _) = magics[v, h];
+                (_, BitBoard mask, _) = args.Item1[v, h];
                 mask = ~mask;
                 
-                BitBoard blockers = BitBoard.Default;
+                BitBoard occupied = BitBoard.Default;
                 while (true) {
                     BitBoard moves = BitBoard.Default;
                     foreach ((int dH, int dV) in deltas) {
-                        (int hI, int vI) = (h, v);
-                        while (!blockers[hI, vI]) {
+                        int hI = h;
+                        int vI = v;
+                        
+                        while (!occupied[hI, vI]) {
                             if (hI + dH is > 7 or < 0 || vI + dV is > 7 or < 0) break;
                             
                             hI += dH;
@@ -260,42 +141,21 @@ namespace Backend.Move
                         }
                     }
 
-                    moveTable[GetMagicIndex(magics, indexBits, blockers, h, v)] = moves;
+                    SlidingMoves[MagicBitBoard.GetMagicIndex(piece, occupied, h, v)] = moves;
 
-                    blockers = (blockers - mask) & mask;
-                    if (blockers.Count == 0) break;
+                    occupied = (occupied - mask) & mask;
+                    if (occupied.Count == 0) break;
                 }
                 
-                Console.WriteLine("Generated sliding moves for [" + (indexBits == ROOK_BITS ? "Rook" : "Bishop") + "]: "
-                                  + (h, v));
+                string chsStr = Util.TupleToChessString((h, v));
+                Console.WriteLine("Generated Black Magic Attack Table for [" + piece + "] at: " + chsStr);
             }
-        }
-
-        private static void SetUp()
-        {
-            GenerateRookMasks();
-            GenerateBishopMasks();
-            
-            GenerateSlidingMoves(SlidingMoves, RookMagic, ROOK_BITS, new []
-            {
-                (1, 0),
-                (0, -1),
-                (-1, 0),
-                (0, 1)
-            });
-            
-            GenerateSlidingMoves(SlidingMoves, BishopMagic, BISHOP_BITS, new []
-            {
-                (1, 1),
-                (1, -1),
-                (-1, -1),
-                (-1, 1)
-            });
         }
 
         static BitLegalMoveSet()
         {
-            SetUp();
+            GenerateSlidingMoves(Piece.Rook);
+            GenerateSlidingMoves(Piece.Bishop);
         }
 
         public BitLegalMoveSet(BitDataBoard board, (int, int) from, bool verify = true)
@@ -370,7 +230,7 @@ namespace Backend.Move
 
         private void LegalRookMoveSet(PieceColor color)
         {
-            int mIndex = GetMagicIndex(RookMagic, ROOK_BITS, ~Board.All(PieceColor.None), H, V);
+            int mIndex = MagicBitBoard.GetMagicIndex(Piece.Rook, ~Board.All(PieceColor.None), H, V);
             Moves |= SlidingMoves[mIndex];
             Moves &= ~Board.All(color);
         }
@@ -383,7 +243,7 @@ namespace Backend.Move
         
         private void LegalBishopMoveSet(PieceColor color)
         {
-            int mIndex = GetMagicIndex(BishopMagic, BISHOP_BITS, ~Board.All(PieceColor.None), H, V);
+            int mIndex = MagicBitBoard.GetMagicIndex(Piece.Bishop, ~Board.All(PieceColor.None), H, V);
             Moves |= SlidingMoves[mIndex];
             Moves &= ~Board.All(color);
         }
