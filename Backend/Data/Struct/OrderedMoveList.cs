01951bcab7b5d3caa8dd32b3d3e435f74318df64
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-05-28 00:59:52 +0500

    Start working on an ordered move list.

---
+++ Backend/Data/Struct/OrderedMoveList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Backend.Data.Struct;
+
+public ref struct OrderedMoveList
+{
+
+    public int Count { get; private set; }
+    
+    private readonly Span<OrderedMoveEntry> Internal;
+
+    public OrderedMoveList(Span<OrderedMoveEntry> @internal)
+    {
+        Internal = @internal;
+        Count = 0;
+    }
+
+    public OrderedMoveEntry this[int i]
+    {
+        get => Internal[i];
+        set
+        {
+            Internal[i] = value;
+            Count++;
+        }
+    }
+
+}
