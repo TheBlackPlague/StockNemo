143bd1ecb581fc7d14542c27c8440d403270e831
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-06-06 16:30:19 +0500

    Implement a Move TranspositionTable to be used in search and move-ordering.

---
+++ Engine/Data/MoveTranspositionTable.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Backend.Data.Struct;
+using Engine.Data.Enum;
+using Engine.Data.Struct;
+
+namespace Engine.Data;
+
+public class MoveTranspositionTable
+{
+
+    private const int MB_TO_B = 1_048_576;
+
+    private readonly int HashFilter;
+    private UnmanagedHeapArray<MoveTranspositionTableEntry> Internal;
+
+    public static MoveTranspositionTable GenerateTable(int megabyteSize) => new(megabyteSize * MB_TO_B);
+
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+    public MoveTranspositionTable(int byteSize)
+    {
+        HashFilter = 0x0;
+
+        for (int i = 0x1; byteSize >= (i + 1) * Marshal.SizeOf<MoveTranspositionTableEntry>(); i = (i << 1) | 0x1) {
+            HashFilter = i;
+        }
+
+        int length = HashFilter + 1;
+        Internal = new UnmanagedHeapArray<MoveTranspositionTableEntry>(length, true);
+
+#if DEBUG
+        Console.WriteLine("Allocated " + Internal.Size() + " bytes for " + length + " TT entries.");
+#endif
+    }
+
+    public ref MoveTranspositionTableEntry this[ulong zobristHash]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            ref MoveTranspositionTableEntry entry = ref Internal[(int)zobristHash & HashFilter];
+            if (entry.ZobristHash == zobristHash) return ref entry;
+            return ref MoveTranspositionTableEntry.Default;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void InsertEntry(ulong zobristHash, MoveTranspositionTableEntryType type, SearchedMove bestMove, byte depth)
+    {
+        int index = (int)zobristHash & HashFilter;
+        ref MoveTranspositionTableEntry entry = ref Internal[index];
+        
+        if (entry.Type == MoveTranspositionTableEntryType.Invalid) {
+            entry.SetEntry(zobristHash, type, ref bestMove, depth);
+            return;
+        }
+        
+        if (entry.Depth - 3 > depth) return;
+        entry.SetEntry(zobristHash, type, ref bestMove, depth);
+    }
+
+    public void FreeMemory()
+    {
+        Internal.Release();
+        Internal = null;
+    }
+
+}
---
+++ Engine/Data/Struct/MoveTranspositionTableEntry.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Engine.Data.Enum;
+
+namespace Engine.Data.Struct;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MoveTranspositionTableEntry
+{
+
+    public static MoveTranspositionTableEntry Default = new();
+    
+    public ulong ZobristHash { get; private set; }
+    public MoveTranspositionTableEntryType Type { get; private set; }
+    public SearchedMove BestMove { get; private set; }
+    public byte Depth { get; private set; }
+
+    public MoveTranspositionTableEntry()
+    {
+        ZobristHash = 0UL;
+        Type = MoveTranspositionTableEntryType.Invalid;
+        BestMove = new SearchedMove();
+        Depth = 0;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void SetEntry(
+        ulong zobristHash, MoveTranspositionTableEntryType type, ref SearchedMove bestMove, byte depth
+    )
+    {
+        ZobristHash = zobristHash;
+        Type = type;
+        BestMove = bestMove;
+        Depth = depth;
+    }
+
+}
