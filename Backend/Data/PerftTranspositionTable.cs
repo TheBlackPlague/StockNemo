fe53e4abe0d3f08db1b135877c10532b066384ae
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-06-18 03:55:26 +0500

    Fill PERFT Transposition Table memory faster using Parallel allocations.

--- Backend/Data/PerftTranspositionTable.cs
+++ Backend/Data/PerftTranspositionTable.cs
@@ -1,5 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Backend.Data;
 
@@ -7,6 +9,7 @@ public class PerftTranspositionTable
 {
 
     private const int HASH_FILTER = 0xFFFFFFF;
+    private const int PARTITION_SIZE = 4096; // 2^12
 
     private readonly PerftTranspositionTableEntry[] Internal = 
         new PerftTranspositionTableEntry[HASH_FILTER + 1];
@@ -15,9 +18,14 @@ public class PerftTranspositionTable
 
     public PerftTranspositionTable()
     {
-        for (int i = 0; i < Internal.Length; i++) {
-            Internal[i] = new PerftTranspositionTableEntry();
-        }
+        int partitionLength = Internal.Length / PARTITION_SIZE;
+        Parallel.For(0, PARTITION_SIZE, p =>
+        {
+            int start = p * partitionLength;
+            int end = start + partitionLength;
+            for (int i = start; i < end; i++)
+                Internal[i] = new PerftTranspositionTableEntry();
+        });
 
         HitCount = 0;
     }
