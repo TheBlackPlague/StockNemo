65c4f884cf7fa43e6a3249c36216663b4d5e8a1f
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-07-02 18:55:56 -0400

    Target version 4.

--- Backend/Version.cs
+++ Backend/Version.cs
@@ -5,7 +5,7 @@ namespace Backend;
 public static class Version
 {
 
-    private const string VERSION = "2.0.0.3";
+    private const string VERSION = "2.0.0.4";
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string Get()
