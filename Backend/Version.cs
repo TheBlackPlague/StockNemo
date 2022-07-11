676c709cd40566127ea516f2b7a60442f1b08e1c
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-07-11 09:09:17 -0400

    Start 3.0.0.0 - major elo gap since 2.0.0.0

--- Backend/Version.cs
+++ Backend/Version.cs
@@ -5,7 +5,7 @@ namespace Backend;
 public static class Version
 {
 
-    private const string VERSION = "2.0.0.9";
+    private const string VERSION = "3.0.0.0";
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string Get()
