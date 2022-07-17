cbb8b86d98ac932ffb41d53198307d15e23e64d8
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-07-17 18:20:33 -0400

    Engine still crashes. Might be due to R being 0 at certain depths and moves. This defaults it to a minimum of 1.
    Bench: 11725377

--- Backend/Data/LogarithmicReductionDepthTable.cs
+++ Backend/Data/LogarithmicReductionDepthTable.cs
@@ -14,7 +14,7 @@ public class LogarithmicReductionDepthTable
     {
         for (int depth = 1; depth < 64; depth++)
         for (int played = 1; played < 64; played++) {
-            Internal[depth * 64 + played] = (int)Math.Log(depth) * (int)Math.Log(played);
+            Internal[depth * 64 + played] = Math.Max((int)Math.Log(depth) * (int)Math.Log(played), 1);
         }
     }
 
