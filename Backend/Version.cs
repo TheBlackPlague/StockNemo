821734b5c38d79a45a175ae9383f7222de1144dd
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-07-20 06:15:48 -0400

    Overwrite version 3.0.0.2.

--- Backend/Version.cs
+++ Backend/Version.cs
@@ -5,7 +5,7 @@ namespace Backend;
 public static class Version
 {
 
-    private const string VERSION = "3.0.0.3";
+    private const string VERSION = "3.0.0.2";
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string Get()
