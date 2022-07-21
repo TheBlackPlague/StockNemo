4f173edf01771069be11a473f789050259a4f482
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-07-21 06:07:08 -0400

    Revert "Trying using faster equality comparison using unions."
    This reverts commit eeef439b511c613f001612686dfc1e2af7b46d15.

--- Backend/Data/Struct/OrderedMoveEntry.cs
+++ Backend/Data/Struct/OrderedMoveEntry.cs
@@ -1,11 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Backend.Data.Enum;
 
 namespace Backend.Data.Struct;
 
 #pragma warning disable CS0660, CS0661
-[StructLayout(LayoutKind.Explicit)]
 public struct OrderedMoveEntry
 #pragma warning restore CS0660, CS0661
 {
@@ -14,21 +12,18 @@ public struct OrderedMoveEntry
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool operator ==(OrderedMoveEntry first, OrderedMoveEntry second) =>
-        first.MovePacked == second.MovePacked;
+        first.From == second.From && first.To == second.To && first.Promotion == second.Promotion;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool operator !=(OrderedMoveEntry first, OrderedMoveEntry second) => !(first == second);
 
-    [FieldOffset(4)] public readonly Square From = Square.Na;
-    [FieldOffset(5)] public readonly Square To = Square.Na;
-    [FieldOffset(6)] public readonly Promotion Promotion = Promotion.None;
-    
-    [FieldOffset(0)] public int Score;
-    [FieldOffset(4)] private readonly int MovePacked;
+    public readonly Square From = Square.Na;
+    public readonly Square To = Square.Na;
+    public readonly Promotion Promotion = Promotion.None;
+    public int Score;
 
     public OrderedMoveEntry(Square from, Square to, Promotion promotion)
     {
-        MovePacked = 0;
         From = from;
         To = to;
         Promotion = promotion;
