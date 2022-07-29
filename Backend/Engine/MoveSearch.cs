4faf3074ece0f9bc7e729f8070ed126f8b698b3a
Shaheryar Sohail <44123859+TheBlackPlague@users.noreply.github.com>
2022-07-29 06:04:22 +0000

    Tune Aspiration Search with Chess Tuning Tools. (#47)
    ELO   | 15.91 +- 8.69 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 3672 W: 1181 L: 1013 D: 1478
    
    Bench: 2808721

--- Backend/Engine/MoveSearch.cs
+++ Backend/Engine/MoveSearch.cs
@@ -19,8 +19,8 @@ public class MoveSearch
     private const int NULL_MOVE_DEPTH = 2;
 
     private const int ASPIRATION_BOUND = 3500;
-    private const int ASPIRATION_SIZE = 50;
-    private const int ASPIRATION_DELTA = 30;
+    private const int ASPIRATION_SIZE = 16;
+    private const int ASPIRATION_DELTA = 23;
     private const int ASPIRATION_DEPTH = 4;
 
     private const int RAZORING_EVALUATION_THRESHOLD = 150;
