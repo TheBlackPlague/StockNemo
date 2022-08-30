0ee48c11657f7ffd5fea0d18ee99c30517736968
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-08-30 03:39:54 -0400

    Replace neural network with the new one trained on 1.2M depth 9 data.
    Bench: 5106914

--- Backend/Engine/Evaluation.cs
+++ Backend/Engine/Evaluation.cs
@@ -12,7 +12,7 @@ public static class Evaluation
 {
 
     private const string NNUE_FILE = "Backend.Engine.NNUE.Model.BasicNNUE";
-    private const string HASH = "f9bccef206";
+    private const string HASH = "00f0765886";
 
     private const int BISHOP_PAIR_EARLY = 25;
     private const int BISHOP_PAIR_LATE = 50;
