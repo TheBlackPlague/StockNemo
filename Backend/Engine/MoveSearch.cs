9282e6f0901250976e8dc3d4fedede007cd6e5d8
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-09-07 23:14:58 -0400

    Implement approximated SEE Pruning in QSearch.
    Bench: 3506664

--- Backend/Engine/MoveSearch.cs
+++ Backend/Engine/MoveSearch.cs
@@ -697,9 +697,21 @@ public class MoveSearch
             // We should being the move that's likely to be the best move at this depth to the top. This ensures
             // that we are searching through the likely best moves first, allowing us to return early.
             moveList.SortNext(i, moveCount);
-                
-            // Make the move.
+            
             OrderedMoveEntry move = moveList[i];
+
+            #region SEE Pruning
+            
+            // Calculate approximation of SEE.
+            int see = SEE.Approximate(board, ref move);
+            
+            // If SEE + positional evaluation margin is greater than beta, then this capture is far too good, and hence
+            // causes a beta cutoff.
+            if (see + earlyEval > beta) return beta;
+            
+            #endregion
+            
+            // Make the move.
             RevertMove rv = board.MoveNNUE(ref move);
             TotalNodeSearchCount++;
         
