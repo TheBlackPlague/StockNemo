c89088bbe42e4d58dca6b76e65caff589f761999
Shaheryar Sohail <44123859+TheBlackPlague@users.noreply.github.com>
2022-09-20 08:47:14 +0000

    Increase neural network training batch size & reliance on game result. (#85)
    ELO   | 124.08 +- 28.34 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 3.00 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 464 W: 244 L: 85 D: 135
    
    ELO   | 131.28 +- 28.85 (95%)
    SPRT  | 60.0+0.60s Threads=1 Hash=256MB
    LLR   | 3.00 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 424 W: 219 L: 66 D: 139
    
    Bench: 12415645
    
    Co-authored-by: UnixWizard <unixwizard@comcast.net>

--- Backend/Engine/Evaluation.cs
+++ Backend/Engine/Evaluation.cs
@@ -13,7 +13,7 @@ public static class Evaluation
 {
 
     private const string NNUE_FILE = "Backend.Engine.NNUE.Model.BasicNNUE";
-    private const string HASH = "02a9cfe493";
+    private const string HASH = "d604f288bd";
 
     private const int BISHOP_PAIR_EARLY = 25;
     private const int BISHOP_PAIR_LATE = 50;
