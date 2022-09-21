a5af915820c16324bab39005381d1d28ce380c9b
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-09-21 12:20:44 -0400

    Try a network with parameters:
    DATA: D9 (1200M) + N5K (500M)
    
    BATCH-SIZE: 8192
    LR: 5e-3
    LR-DROP: 20
    WDL: 0.2
    EPOCHS: 80
    
    Bench: 12567543

--- Backend/Engine/Evaluation.cs
+++ Backend/Engine/Evaluation.cs
@@ -13,7 +13,7 @@ public static class Evaluation
 {
 
     private const string NNUE_FILE = "Backend.Engine.NNUE.Model.BasicNNUE";
-    private const string HASH = "d604f288bd";
+    private const string HASH = "97f7a88947";
 
     private const int BISHOP_PAIR_EARLY = 25;
     private const int BISHOP_PAIR_LATE = 50;
